66ea0e47a1ba58fd10373670f55524478c8036ab
Robert Alan Gemong <snailbob@live.com>
2025-01-24 01:28:39 +0800

    aboutMe final

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,14 +1,14 @@
 export const aboutMeData = [
   {
     id: 1,
-    bio: "Hi, I am Gemong - energetic, dependable and an innovative problem solver. I am proficient in HTML, CSS, and JavaScript, which allows me to create interactive, responsive, and visually appealing user interfaces. I have a strong attention to detail, ensuring that every element on a webpage aligns perfectly and adheres to design guidelines. Additionally, I'm highly adaptable and always eager to learn new technologies and framework. I am passionate about staying up-to-date with the latest web development trends.",
+    bio: "Hi, I am Gemong - energetic, dependable and an innovative problem solver. I am a dedicated and detail-oriented web developer with a strong specialization in front-end development. I thrive on creating visually appealing, user-friendly, and responsive web interfaces that deliver seamless experiences across devices. My expertise includes HTML, CSS, and JavaScript, complemented by proficiency in modern frameworks and libraries like React, Angular, or Vue.js.",
   },
   {
     id: 2,
-    bio: "When I'm not immersed in lines of code and web development, you'll often find me strumming my guitar and getting lost in the rhythm of music. Playing guitar has been a passion of mine for as long as I can remember. It's my creative escape, a way to express emotions and tell stories.",
+    bio: "Additionally, I'm highly adaptable and always eager to learn new technologies and frameworks to stay up-to-date with industry trends. My strong communication skills enable me to effectively collaborate with designers, back-end developers, and other team members to deliver seamless user experiences.",
   },
   {
-    id: 3,
-    bio: "When I'm not playing music, I find solace in the great outdoors. Nature has a way of refreshing the mind and rejuvenating the spirit. I enjoy hiking through scenic trails, breathing in the fresh air, and taking in the beauty of the natural world.",
+    id: 5,
+    bio: "When I'm not immersed in lines of code and web development, you'll often find me strumming my guitar and getting lost in the rhythm of music. I also find solace in the great outdoors. Nature has a way of refreshing the mind and rejuvenating the spirit. I enjoy hiking through scenic trails and breathing in the fresh air.",
   },
 ];
